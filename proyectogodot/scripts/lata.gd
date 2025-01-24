54cafb3fb9002e2f5ba2ed5d165db10e03dadb8e
LuisEnriqueCabelloBlanco <luisecab@ucm.es>
2025-01-24 23:41:07 +0100

    Chapa y Pintura

--- proyectogodot/scripts/lata.gd
+++ proyectogodot/scripts/lata.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 var isPicked : bool
 var isMoused : bool
+var is_draggin: bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,26 +12,17 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
-
-func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	if isPicked:
-		position = get_viewport().get_mouse_position()
 
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.pressed and isMoused:
-			Pick()
-		if !event.pressed and isPicked:
-			Drop()
+		if !event.is_pressed():
+			is_draggin = false
+	pass
 
-func Drop():
-	isPicked = false
-
-func Pick():
-	isPicked = true
+func _physics_process(delta: float) -> void:
+	if(is_draggin):
+		global_transform.origin = get_global_mouse_position()
 
 func _on_body_entered(body: Node) -> void:
 	print_debug("lata collision")
@@ -44,3 +36,10 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	print_debug("mouse exited")
 	isMoused = false
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			is_draggin = true
+	pass # Replace with function body.
