34065be790d0567fe21bce2aa9cf7b2d36f1eb62
Diego M <you@example.com>
2025-01-24 20:58:57 +0100

    extends node

--- proyectogodot/scripts/lata.gd
+++ proyectogodot/scripts/lata.gd
@@ -1,19 +1,24 @@
 extends RigidBody2D
 
+var isPicked : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	isPicked = false
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if isPicked:
+		position = get_viewport().get_mouse_position()
 
 
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	print_debug("lata input")
-
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		isPicked = true;
+	
+		
 
 func _on_body_entered(body: Node) -> void:
 	print_debug("lata collision")
