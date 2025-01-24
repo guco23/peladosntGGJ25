e559c38b6428486db4451d802d015d6f706c7135
SamuelGarciaBazan <samuga05@ucm.es>
2025-01-24 23:52:39 +0100

    frisby spawner

--- proyectogodot/scripts/frisby.gd
+++ proyectogodot/scripts/frisby.gd
@@ -3,6 +3,10 @@ extends RigidBody2D
 
 @export var targetNode:Node2D 
 @export var velocityModule:float
+@export var lifeTime:float
+
+
+var currLifeTime:float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,8 +29,32 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
+	currLifeTime += delta
 	
-	
-	
+	if currLifeTime > lifeTime:
+		queue_free()
+		
 	
 	pass
+
+func isInLayer(layerBody:int, layerTarget:int) -> bool:
+	
+	#print_debug("estoy aqui", layerBody,layerTarget)
+	
+	if layerBody & (1 << (layerTarget-1)):
+		return true
+	else:
+		return false
+
+func _on_body_entered(body:Node) -> void:
+	
+	if body is CollisionObject2D:
+		if isInLayer(body.collision_layer,3):
+			print_debug("he chocado con pared, destruccion")
+			#queue_free()
+		
+		
+	
+	print_debug("he chocao")
+	
+	pass # Replace with function body.
--- proyectogodot/scripts/frisby_spawner.gd
+++ proyectogodot/scripts/frisby_spawner.gd
@@ -1,7 +1,35 @@
 class_name FrisbySpawner
 extends Node2D
 
+const FRISBY:PackedScene = preload("res://prefabs/frisby.tscn")
+@export var target:Node2D 
 
+@export var minSpawnTime:float = 2
+@export var maxSpawnTime:float = 3
+
+var spawnTime:float = 0
+var spawnTimeCounter = 0
+
+
+func spawnFrisby():
+	
+	
+	var newFrisby = FRISBY.instantiate()
+	
+	newFrisby.position = Vector2(0,0)
+	newFrisby.targetNode = target
+	
+	add_child(newFrisby)
+	
+	print_debug("frisby instanciado")
+	
+	pass
+	
+	
+func calcultateNewSpawnTime():
+	spawnTime = randf_range(minSpawnTime,maxSpawnTime)
+	spawnTimeCounter = 0
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,4 +37,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	spawnTimeCounter += delta
+	
+	if spawnTimeCounter > spawnTime:
+		spawnFrisby()
+		calcultateNewSpawnTime()
+		
+		
+	
 	pass
