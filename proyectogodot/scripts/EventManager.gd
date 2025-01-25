0b71a8fed796ceaba5b26d012417e1cf51d095b0
Diego M <you@example.com>
2025-01-25 16:46:22 +0100

    pero que locuraaaa

--- proyectogodot/scripts/EventManager.gd
+++ proyectogodot/scripts/EventManager.gd
@@ -15,9 +15,11 @@ extends Node
 
 var moscaPref : PackedScene = preload("res://prefabs/Mosca.tscn") #El prefab de la mosca
 var discoPref : PackedScene = preload("res://prefabs/disco.tscn") #El prefab del disco (solo puede haber un disco)
+var manchaPref : PackedScene = preload("res://prefabs/Spot.tscn") #El prefab de la mancha
+
 @export var mano : Node2D #La mano en la escena
 @export var frisbeeSpawner : Node2D
-@export var luz : Node2D
+@export var interruptorYfondo : Node2D
 
 var rand = RandomNumberGenerator.new()
 var totalWeight : int
@@ -57,19 +59,14 @@ func LaunchEvent():
 		GeneraMosca()
 	elif val <= discoWeight:
 		GeneraDisco()
-		pass
 	elif val <= manoWeight:
 		LlamaMano()
-		pass
 	elif val <= frisbeeWeight:
 		LlamaFrisbee()
-		pass
 	elif val <= luzWeight:
 		ApagaLuz()
-		pass
 	elif val <= manchaWeight:
 		Manchar()
-		pass
 	#Selecciona aleatoriamente un evento de la lista de eventos
 
 func GeneraMosca():
@@ -90,7 +87,10 @@ func LlamaFrisbee():
 	frisbeeSpawner.spawnFrisby()
 
 func ApagaLuz():
-	print_debug("apaga luz")
+	interruptorYfondo.get_child(0).apagarLuz()
 
 func Manchar():
-	print_debug("mancha")
+	var mancha = manchaPref.instantiate()
+	mancha.position = get_viewport().size / 2
+	#todo
+	add_sibling(mancha)
