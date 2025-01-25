8ff8c358f92fd304217649e8721ec03f930ed003
SamuelGarciaBazan <samuga05@ucm.es>
2025-01-25 01:51:33 +0100

    interruptor

---
+++ proyectogodot/prefabs/fondo_negro.gd
@@ -0,0 +1,22 @@
+class_name fondo_negro
+
+extends Sprite2D
+
+
+
+func apagarLuz():
+	visible = true
+
+func encenderLuz():
+	visible = false
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#encenderLuz()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
