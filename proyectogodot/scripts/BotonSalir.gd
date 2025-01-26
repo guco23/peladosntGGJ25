a939d8d223843cdbdf231437cbf7fd675168d911
Diego M <you@example.com>
2025-01-26 08:11:58 +0100

    arreglillos botones

--- proyectogodot/scripts/BotonSalir.gd
+++ proyectogodot/scripts/BotonSalir.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton and event.is_pressed():
 		get_tree().quit(0)
