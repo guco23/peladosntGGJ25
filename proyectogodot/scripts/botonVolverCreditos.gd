70384f5738019e3add7ade91b35f61c70d56716a
Diego M <you@example.com>
2025-01-26 08:07:34 +0100

    pantalla de creditos

---
+++ proyectogodot/scripts/botonVolverCreditos.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
