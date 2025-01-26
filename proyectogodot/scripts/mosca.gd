55fde313fdbc76692c4391a61b9b2d386e68ef92
SamuelGarciaBazan <samuga05@ucm.es>
2025-01-26 10:44:38 +0100

    mosca y mancha

--- proyectogodot/scripts/mosca.gd
+++ proyectogodot/scripts/mosca.gd
@@ -7,22 +7,64 @@ extends StaticBody2D
 @export var offset:Vector2 = Vector2(0,50)
 var rng = RandomNumberGenerator.new()
 
+
+@export var mainMenu:bool 
+
+var targetPos:Vector2
+
+var closeUmbral:float = 10
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_node("Sprite2D").play()
+	
+	
+	targetPos = Vector2(\
+			randf_range(100,get_viewport_rect().size.x -100),
+			randf_range(100,get_viewport_rect().size.y -100))
+			
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var val=rng.randf_range(-variablity,variablity)
-	position += ((playerRef.position+offset-position).normalized()*moscaSpeed+Vector2(0,variablity))*delta
 	
+	if !mainMenu:
+		var val=rng.randf_range(-variablity,variablity)
+		position += ((playerRef.position+offset-position).normalized()*moscaSpeed+Vector2(0,variablity))*delta
+		
+		
+		if(position.x < playerRef.position.x):
+			get_child(0).flip_h = true
+		else: 
+			get_child(0).flip_h = false
 	
-	if(position.x < playerRef.position.x):
-		get_child(0).flip_h = true
-	else: 
-		get_child(0).flip_h = false
+	else:
+		
+		if ((targetPos-position).length()) < closeUmbral:
+						
+			targetPos = Vector2(\
+			randf_range(100,get_viewport_rect().size.x -100),
+			randf_range(100,get_viewport_rect().size.y -100))
+			
+		else:
+			
+			var dir = targetPos  -position
+			
+			position += dir.normalized()*moscaSpeed*delta
+			print_debug(dir)
+		
+		
+		if(position.x < targetPos.x):
+			get_child(0).flip_h = true
+		else: 
+			get_child(0).flip_h = false
+		
+		
+		pass
+	
 	
 	pass
 
