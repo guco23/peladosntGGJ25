d723419f9acf1c825e6e94fb93a592a30ab0319e
Diego M <you@example.com>
2025-01-26 01:53:00 +0100

    no se que esta pasando

--- proyectogodot/scripts/buble_spawner.gd
+++ proyectogodot/scripts/buble_spawner.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
 var bubles:PackedScene=preload("res://prefabs/Burbuja.tscn")
+var gen:RandomNumberGenerator
+@export var offsetChance:Vector2
+
+func _ready() -> void:
+	gen = RandomNumberGenerator.new()
 
 func SpawnBuble():
 	var buble:Node2D = bubles.instantiate()
 	get_parent().add_sibling(buble)
-	buble.global_position = global_position
+	var aux = Vector2(gen.randf_range(-offsetChance.x,offsetChance.x),gen.randf_range(-offsetChance.y,offsetChance.y))
+	buble.global_position = global_position+aux
 
 func _on_lata_gas_up(gasValue: Variant) -> void:
 	if(gasValue > GameManager.gasThreshold):
-		SpawnBuble()
+		for i in range(2):
+			SpawnBuble()
 	pass # Replace with function body.
