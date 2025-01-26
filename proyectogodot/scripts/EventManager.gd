9d408af8a2fa0b3820d05c05c4696e692fc33339
SamuelGarciaBazan <samuga05@ucm.es>
2025-01-26 01:17:42 +0100

    spawn de lamparas

--- proyectogodot/scripts/EventManager.gd
+++ proyectogodot/scripts/EventManager.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 @export var minTime : float #El tiempo minimo para nuevo vento
 @export var startTime : float #El tiempo inicial de nuevo evento
@@ -19,6 +19,10 @@ var moscaPref : PackedScene = preload("res://prefabs/Mosca.tscn") #El prefab de 
 var discoPref : PackedScene = preload("res://prefabs/disco.tscn") #El prefab del disco (solo puede haber un disco)
 var manchaPref : PackedScene = preload("res://prefabs/Spot.tscn") #El prefab de la mancha
 var ovniPref : PackedScene = preload("res://prefabs/ovni.tscn")
+var lampPref : PackedScene = preload("res://prefabs/Lampara.tscn")
+
+
+
 
 @export var mano : Node2D #La mano en la escena
 @export var frisbeeSpawner : Node2D
@@ -113,5 +117,28 @@ func Manchar():
 
 func TiraLampara():
 	
+	
+	var nuevaLamp: = lampPref.instantiate()
+	
+	add_child(nuevaLamp)
+	
+	
+	
+	
+	nuevaLamp.position = \
+	 Vector2( randf_range( 300 ,get_viewport_rect().size.x- 300),110)
+	
+	var timer = Timer.new()
+	timer.one_shot = true 
+	timer.wait_time = 3
+
+	add_child(timer)
+
+	timer.connect("timeout",nuevaLamp.Fall)
+
+	timer.start()
+	
 	if lampara != null:
 		lampara.Fall()
+		
+		
